ace295a14374cbfd9848fd8525822e4268fc9a68
jzh <1499765600@qq.com>
2022-12-11 23:43:48 +0800

    教师删除公告完成

--- databasehomework/src/page/teaAddScore.vue
+++ databasehomework/src/page/teaAddScore.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>
+  <div class="main">
+    <div style="text-align: center">
+      <div style="font-weight: bold; font-size: large; margin-bottom: 20px">课程</div>
+      <div>
+        课程名：{{cousename}}
+      </div>
+      <div>
+        课程号：{{courseNum}}
+      </div>
+      <div >
+        <div style="text-align: center; width:1200px">
+          <el-table :data="reslist" stripe style="width: 100%">
+            <el-table-column prop="courseuuid" label="id" width="100" />
+            <el-table-column prop="courseName" label="学生名字" width="100" />
+            <el-table-column prop="bixiu" label="学生性别" />
+            <el-table-column prop="credit" label="学生专业" />
+            <el-table-column prop="checkscore" label="学生班级" />
+            <el-table-column label="选择" width="200">
+              <template #default="scope">
+                <el-button @click="gototeaAddScore(scope.row)" v-show="this.from == 1">修改成绩</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+      </div>
+    </div>
+  </div>
+  <el-dialog
+    v-model="dialogVisible"
+    title="Tips"
+    width="30%"
+    :before-close="handleClose"
+  >
     <div>
       <el-form-item label="课程名">
         <el-input v-model="courseName" />
@@ -17,13 +49,50 @@
         <el-input v-model="examscore" />
       </el-form-item>
     </div>
-    <el-button type="primary" @click="addnotice">发布</el-button>
-  </div>
+    <el-button type="primary" @click="addscore">发布</el-button>
+  </el-dialog>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "teaAddScore"
+  name: "teaAddScore",
+  data(){
+    return{
+      cousename:null,
+      courseNum:null,
+      from: 0,
+      classInfo: null,
+      stuuuid : null,
+      dialogVisible: false
+    }
+  },
+  mounted() {
+    this.classInfo =  JSON.parse(this.$route.query.classInfo);
+    this.from =  JSON.parse(this.$route.query.from);
+    let data = new FormData();
+    let url = "";
+    let config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        "token": this.$store.state.token
+      },
+    };
+  //  获取信息，如果从1来可以修改，如果从0来不可以修改
+
+  },
+  methods:{
+    gototeaAddScore(a)
+    {
+      //打开添加成绩窗口
+      this.dialogVisible = true;
+    }
+    addscore()
+    {
+    //  添加成绩
+    }
+  }
 };
 </script>
 
--- databasehomework/src/page/teaGetCourse.vue
+++ databasehomework/src/page/teaGetCourse.vue
@@ -1,10 +1,93 @@
 <template>
-
+  <div class="main">
+    <div style="text-align: center">
+      <div style="font-weight: bold; font-size: large; margin-bottom: 20px">主教师课程</div>
+      <div >
+        <div style="text-align: center; width:1200px">
+          <el-table :data="reslist" stripe style="width: 100%">
+            <el-table-column prop="courseuuid" label="id" width="100" />
+            <el-table-column prop="courseName" label="课程名" width="100" />
+            <el-table-column prop="bixiu" label="是否必修" />
+            <el-table-column prop="credit" label="学分" />
+            <el-table-column prop="checkscore" label="签到成绩" />
+            <el-table-column prop="dailyscore" label="平时成绩" />
+            <el-table-column prop="examscore" label="考试成绩" />
+            <el-table-column prop="score" label="总成绩" />
+            <el-table-column label="选择" width="200">
+              <template #default="scope">
+                <el-button @click="gototeaAddScore(scope.row, 1)" v-show="scope.row.score == 1">进入</el-button>
+                <el-popconfirm title="是否要删除"
+                               confirm-button-text="是"
+                               cancel-button-text="否"
+                               @confirm="deleteCourse(scope.row)">
+                  <template #reference>
+                    <el-button v-show="scope.row.score == 0" type="danger">删除课程</el-button>
+                  </template>
+                </el-popconfirm>
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+      </div>
+    </div>
+<!--    <div style="font-weight: bold; font-size: large; margin-bottom: 20px">副教师课程</div>-->
+<!--    <div >-->
+<!--      <div style="text-align: center; width:1200px">-->
+<!--        <el-table :data="reslist" stripe style="width: 100%">-->
+<!--          <el-table-column prop="courseuuid" label="id" width="100" />-->
+<!--          <el-table-column prop="courseName" label="课程名" width="100" />-->
+<!--          <el-table-column prop="bixiu" label="是否必修" />-->
+<!--          <el-table-column prop="credit" label="学分" />-->
+<!--          <el-table-column prop="checkscore" label="签到成绩" />-->
+<!--          <el-table-column prop="dailyscore" label="平时成绩" />-->
+<!--          <el-table-column prop="examscore" label="考试成绩" />-->
+<!--          <el-table-column prop="score" label="总成绩" />-->
+<!--          <el-table-column label="选择" width="200">-->
+<!--            <template #default="scope">-->
+<!--              <el-button @click="gotopingfen(scope.row)" v-show="scope.row.score == 1">进入</el-button>-->
+<!--              <el-popconfirm title="是否要删除"-->
+<!--                             confirm-button-text="是"-->
+<!--                             cancel-button-text="否"-->
+<!--                             @confirm="deleteCourse(scope.row)">-->
+<!--                <template #reference>-->
+<!--                  <el-button v-show="scope.row.score == 0" type="danger">删除课程</el-button>-->
+<!--                </template>-->
+<!--              </el-popconfirm>-->
+<!--            </template>-->
+<!--          </el-table-column>-->
+<!--        </el-table>-->
+<!--      </div>-->
+<!--    </div>-->
+  </div>
 </template>
 
 <script>
 export default {
-  name: "teaGetCourse"
+  name: "teaGetCourse",
+  data()
+  {
+    return{
+
+    }
+  },
+  mounted() {
+  },
+  methods:{
+    gototeaAddScore(a,b)
+    {
+      this.$router.push({
+        path:"/teaAddScore",
+        query: {
+          classInfo: JSON.stringify(a),
+          from: JSON.stringify(b)
+        }
+      })
+    },
+    deleteCourse(e)
+    {
+      console.log(e);
+    }
+  }
 };
 </script>
 
